866b8a1c8a2788359a5a0f052e58c0f8a8b519a0
Caio Bianchi <caiohbs23@gmail.com>
2024-06-27 10:51:16 -0300

    add code for exiting terminal to end of scripts

--- a-remove.sh
+++ a-remove.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
 sudo apt-get autoremove
+
+echo -e "\033[0;36m\nClose this terminal? (Y/n)\033[0m"
+read answer
+
+case $answer in
+    [Yy])
+        kill -9 $PPID
+        ;;
+    [Nn])
+        ;;
+    *)
+        echo -e "\033[0;31m\nInvalid response.\033[0m"
+        ;;
+esac
--- update.sh
+++ update.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
 sudo apt-get update
 
 sudo apt-get upgrade
+
+echo -e "\033[0;36m\nClose this terminal? (Y/n)\033[0m"
+read answer
+
+case $answer in
+    [Yy])
+        kill -9 $PPID
+        ;;
+    [Nn])
+        ;;
+    *)
+        echo -e "\033[0;31m\nInvalid response.\033[0m"
+        ;;
+esac
