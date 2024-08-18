ae4408ff928d84ebbdc45702837f7c2d63952470
Caio Bianchi <caiohbs23@gmail.com>
2024-08-18 16:54:16 -0300

    added new script

---
+++ src/make-a-note.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# "Notes" folder check
+notes_folder="$HOME/Notes"
+
+if [ ! -d "$notes_folder" ]; then
+    mkdir -p "$notes_folder"
+    echo
+    echo "\"Notes\" folder created successfully"
+    echo
+fi
+
+# Note name
+date=$(date +"%m-%d_")
+curr_time=$(date +"%H")
+
+if [ "$curr_time" -lt 12 ]; then
+    rotation="manha"
+else
+    rotation="tarde"
+fi
+
+full_file_path="${notes_folder}/${date}${rotation}"
+
+# Check if file exists
+if [ ! -f "$full_file_path" ]; then
+    touch "${full_file_path}.txt"
+fi
+
+#Open file
+nohup gedit "${full_file_path}" &
+exit
